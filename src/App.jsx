04d668e938b9e6228cc8556f0644dfdfa1ab6ceb
umesh30370 <vadiselaumesh55@gmail.com>
2025-08-11 12:19:14 +0530

    Setup for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -18,32 +18,32 @@ class App extends Component {
         <section>
           {/* Each card displays an image and a label/title below it */}
           <div className='card'>
-            <img src='/1.jpeg' alt='' />
+            <img src='/table/public/1.jpeg' alt='' />
             <label>Project One</label>
           </div>
 
           <div className='card'>
-            <img src='/2.jpeg' alt='' />
+            <img src='/table/public/2.jpeg' alt='' />
             <label>Project Two</label>
           </div>
 
           <div className='card'>
-            <img src='/3.jpeg' alt='' />
+            <img src='/table/public/3.jpeg' alt='' />
             <label>Project Three</label>
           </div>
 
           <div className='card'>
-            <img src='/4.jpeg' alt='' />
+            <img src='/table/public/4.jpeg' alt='' />
             <label>Project Four</label>
           </div>
 
           <div className='card'>
-            <img src='/5.jpeg' alt='' />
+            <img src='/table/public/5.jpeg' alt='' />
             <label>Project Five</label>
           </div>
 
           <div className='card'>
-            <img src='/6.jpeg' alt='' />
+            <img src='/table/public/6.jpeg' alt='' />
             <label>Project Six</label>
           </div>
         </section>
